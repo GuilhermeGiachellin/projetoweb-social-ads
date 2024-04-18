233d25e2ba6d0f6557bce8f71480040de11b7100
Guilherme Giachellin <guilherme.giache@gmail.com>
2024-04-17 22:35:56 -0300

    FirstCommit

---
+++ RedeSocial/WebSite/Models/AnuncioModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebSite.Models
+{
+    public class AnuncioModel
+    {
+        public string Codigo { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco {  get; set; }
+    }
+}
